7624f62cc8ec19b448cb0b05249fee9a45907efc
CBH-23 <codingbyher23@gmail.com>
2025-02-17 16:11:24 +0000

    Enable debug mode and add new views and templates for post management

--- chic_nerd/urls.py
+++ chic_nerd/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', 
-        views.HomePage.as_view(), name='home'),
+    path('', views.HomePage.as_view(), name='home'),
+    path('post/', views.PostPage.as_view(), name='post'),
+    path('login/', views.LoginPage.as_view(), name='login'),
+    path('feed/', views.FeedPage.as_view(), name='feed'),
 ]
--- chic_nerd/views.py
+++ chic_nerd/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
-from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
     """
     Displays home page"
     """
     template_name = 'index.html'
+
+class PostPage(TemplateView):
+    template_name = 'post.html'
+
+class LoginPage(TemplateView):
+    template_name = 'login.html'
+
+class FeedPage(TemplateView):
+    template_name = 'feed.html'
